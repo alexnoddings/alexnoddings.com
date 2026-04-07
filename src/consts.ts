a1e7ddca27a9f38ec38f44878d92dee3db856129
Alex Noddings <alexnoddings@users.noreply.github.com>
2026-04-07 22:17:01 +0100

    Update site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = 'Alex Noddings';
-export const SITE_DESCRIPTION = 'Senior full-stack Software Engineer architecting and leading the development of complex business systems'
+export const SITE_DESCRIPTION = 'Senior full-stack Software Engineer. I build high-quality, performant, accessible web systems.'
